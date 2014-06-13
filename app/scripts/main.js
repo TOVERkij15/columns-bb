cef08036fc3a903c9e7d61b5505bfa9f14dc98dd
tasha kijsamnong <tkijsamnong@yahoo.com>
2014-06-13 16:44:29 -0400

    Re-fact/done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,46 +9,40 @@
 
 var Photo = Backbone.Model.extend({
 //specify the default attributes for model.
+	idAttribute: "_id",
+
 	defaults: {
     	url: '',
 	},
-//tells backbone that mongo(database) is calling the id something else.
-	idAttribute: '_id',
-
-
 });
-
-
 
 //////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////COLLECTION://///////////////////////////////
 //collection can fetch things at once and run them automatically from the server.
 //The Collection will take every object literal and try to run them through the constructor
-var PhotoCollection1 = Backbone.Collection.extend({
+var PhotoCollection = Backbone.Collection.extend({
 //In reference to Model	
 	model: Photo,
-	url: 'http://tiny-pizza-server.herokuapp.com/collections/photos',
+	
+	initialize: function(options) {
+		this.url = options.url;
 
-});
-
-var PhotoCollection2 = Backbone.Collection.extend({
-	model: Photo,	
-  	url: 'http://tiny-pizza-server.herokuapp.com/collections/photos2',
-
-});
-
-var PhotoCollection3 = Backbone.Collection.extend({
-	model: Photo,	
-  	url: 'http://tiny-pizza-server.herokuapp.com/collections/photos3',
-
-});
+		this.id = _.uniqueId('collection-');
+		this.on('add',function(photo){
+			new PhotoView({
+				model:photo,
+				container:$('.' + this.id)
+			});
+		})
+	}
+})
 
 
 ///////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////View/////////////////////////////////////////
 //VIEW:
 //Forces settings into model.Takes the same data in model and makes changes.
-var PhotoViewOne = Backbone.View.extend({
+var PhotoView = Backbone.View.extend({
 	
 	tagName: "nav",
 
@@ -60,183 +54,94 @@ var PhotoViewOne = Backbone.View.extend({
 //The callback will be invoked whenever the event is fired.
 //click.class for updates and changes
 	events: {
-	    "click .remove-photo": "deletePhoto",
-	    "click .store-photo": "savePhoto",
-	    "click .move-center": "movePhoto",
-	    "click .add-photo":   "addPhoto"
-	   
-  	},
+	    "click .remove-photo": 		"deletePhoto",
+	    "click .move-right-once": 	"movePhoto",
+	    "click .move-right-twice": 	"movePhoto2",
+	},
 //Listento tells an object to listen to a particular event on another object. 
 //When the model is created, the initialized function will be invoked.
 //Refreshes when there is a change in the model. Renders if there is a change to the container.
-  	initialize: function(){
-  		//append everything within the view
-  		this.listenTo(this.model, 'change', this.render),
-  		$('.collection1').append(this.el);
-  		//this renders immediately w/o listing to when called.
-  		this.render()
-  	},
+  	
 
-  	render: function(){
+	initialize: function(options){
+
+		this.container = options.container
+
+		this.listenTo(this.model,'destroy',this.remove);
+		this.listenTo(this.model,'change',this.render);
+
+		this.container.append(this.el);
+		this.render();
+	},
+
+	render: function(){
   		var renderedtemplate = this.template(this.model.attributes)
     	this.$el.html(renderedtemplate);
+    	return this;
   		
   	},
-	
-	addPhoto: function(){
 
+  	deletePhoto: function(){
 
-	},
-	
+  		this.model.destroy();
+  	},
+
+//movePhoto: will move once. 
+//movePhoto2: will move twice. 
 	movePhoto: function(){
-    	$.post('http://tiny-pizza-server.herokuapp.com/collections/photos2', {
-        url: this.model.attributes.url,
-    });
-    	this.model.destroy().done(function(){
-      	$('.collection2').html('');
-      	$('.collection2').append('');
-      	var app = new AppRouter;
-
-    })
-  },
-
-	savePhoto: function(){
-//find:class
-	var fieldvalue = this.$el.find('.field input').val();
-	  	console.log(fieldvalue);
-	  
-	  //setting the new propety value of model locally
-	  	this.model.set('Photo', fieldvalue);
-	 //saves the model
-	  	this.model.save()
+	//the index is gettin the index of the collection array. 	
+		var index = _.indexOf(collectionArray, this.model.collection);
+		console.log('tester', index)
+		
+		if (index + 1 >= collectionArray.length){
+			var newModel= collectionArray[0].add({url: this.model.attributes.url})
+			console.log('yes!')
+		}else{
+			var newModel= collectionArray[index + 1].add({url: this.model.attributes.url})
+			console.log('what?')
+		}
 	
+		newModel.save();
+		this.model.destroy();
+	},
+
+	movePhoto2: function(){
+		var index = _.indexOf(collectionArray, this.model.collection);
+		console.log('work!', index)
+
+		if (index + 2 >= collectionArray.length){
+			collectionArray[1].add({url: this.model.attributes.url})
+			console.log('maybe?')
+		}else{
+			collectionArray[index + 2].add({url: this.model.attributes.url})
+			console.log('yay!')
+		}
+
+		newModel.save();
+		this.model.destroy();
 	},
 });
 
-////////////////////Column2////////////////////////////////////////////////////
-var PhotoViewTwo = Backbone.View.extend({
+var collectionArray = [
+	new PhotoCollection({url:'http://tiny-pizza-server.herokuapp.com/collections/photos'}),
+	new PhotoCollection({url:'http://tiny-pizza-server.herokuapp.com/collections/photos1'}),
+	new PhotoCollection({url:'http://tiny-pizza-server.herokuapp.com/collections/photos2'})
+]
+ 
+collectionArray.forEach(function (images){
+	var newDiv = $('<div></div>')
+	newDiv.addClass(images.id + ' container');
+	$('.image-container').append(newDiv);
 
-  twoTemplate: _.template($('.column-two-template').text()),
+	images.fetch();
 
-  tagName: "nav",
-  	
-  	events: {
-	    "click .remove-photo": "deletePhoto",
-	    "click .move-right": "moveRight",
-	   
-  	},
-
-  	initialize: function(){
-  		this.listenTo(this.model, 'change', this.render),
-  		$('.collection2').append(this.el);
-  		this.render();
-  		
-  	},
-
-  	render: function(){
-    	var renderedtwoTemplate = this.twoTemplate(this.model.attributes)
-    	this.$el.html(renderedtwoTemplate);
-  		
-  	},
-
-  	moveRight: function(){
-    	$.post('http://tiny-pizza-server.herokuapp.com/collections/photos3', {
-        url: this.model.attributes.url,
-    });
-    	this.model.destroy().done(function(){
-      	$('.collection3').html('');
-      	$('.collection3').append('');
-      	var app = new AppRouter;
-
-    })
-},
-
-});
-
-//////////////////third Column/////////////////////////////////////////////
-
-var PhotoViewThree = Backbone.View.extend({
-
-  threeTemplate: _.template($('.column-three-template').text()),
-  	
-  	tagName: "nav",
-
-  	events: {
-	    "click .remove-photo": "deletePhoto",
-	    "click .move-back": "movePhoto3",
-	   
-  	},
-
-  	initialize: function(){
-  		this.listenTo(this.model, 'destroy', this.render),
-  		$('.collection3').append(this.el);
-  		this.render();
-  	},
-
-  	render: function(){
-    	var renderedthreeTemplate = this.threeTemplate(this.model.attributes)
-    	this.$el.html(renderedthreeTemplate);
-  		
-  	},
-
-  	movePhoto3: function(){
-    	$.post('http://tiny-pizza-server.herokuapp.com/collections/photos2', {
-        url: this.model.attributes.url,
-    });
-    	
-    	this.model.destroy().done(function(){
-      	$('.collection3').html('');
-      	$('.collection3').append('');
-      	var app = new AppRouter;
-      
-  	
-  		})
-  	
-	},
-
-});
-
-//////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////Router////////////////////////////////
-
-var leftPhoto = new PhotoCollection1();
-var centerPhoto = new PhotoCollection2();
-var rightPhoto 	= new PhotoCollection3();
-
-var AppRouter = Backbone.Router.extend({
 	
- 	initialize: function(){
- 		console.log ('router initialized');
-
-		leftPhoto.fetch().done(function() {
-			leftPhoto.each(function (photo) {
-				new PhotoViewOne({ model: photo });
-			})
-		});
-	
-		centerPhoto.fetch().done(function() {
-			centerPhoto.each(function (photo) {
-				new PhotoViewTwo({ model: photo });
-			})
-		});
-	
-
-		rightPhoto.fetch().done(function() {
-			rightPhoto.each(function (photo) {
-				new PhotoViewThree({ model: photo });
-			})
-		});
-	},	
 })
 
 
 
-var addPhoto = new PhotoCollection1;
-var addPhoto = new PhotoCollection2;
-var addPhoto = new PhotoCollection3;	
 
-var Router = new AppRouter;
-Backbone.history.start();
+
 
 
 
